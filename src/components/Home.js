abdb1d89e44250aa325b3219289a79941bddc206
Edgar Serna <eserna03@gmail.com>
2019-04-14 19:00:48 -0500

    Base ReactJS project

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Button } from '@material-ui/core';
+
+class Home extends Component {
+  render() {
+    return (
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    );
+  }
+}
+
+export default Home;
